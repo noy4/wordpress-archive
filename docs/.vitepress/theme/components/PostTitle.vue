f7df17a177a196e85541c8b945a26cb66b67ecaa
noy4 <naoyan418@gmail.com>
2025-02-28 05:37:37 +0900

    PostTitle

---
+++ docs/.vitepress/theme/components/PostTitle.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useData } from 'vitepress'
+
+const { frontmatter } = useData()
+const date = new Date(frontmatter.value.date).toLocaleDateString('ja-JP')
+</script>
+
+<template>
+  <div class="vp-doc">
+    <h1>{{ frontmatter.title }}</h1>
+    <p>
+      <a
+        v-for="category in frontmatter.categories"
+        :key="category"
+      >
+        {{ category }}
+      </a>
+      <a
+        v-for="tag in frontmatter.tags"
+        :key="tag"
+      >
+        #{{ tag }}
+      </a>
+    </p>
+
+    <p>
+      <span>{{ date }}</span>
+    </p>
+  </div>
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,25 @@
 import type { Theme } from 'vitepress'
+import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
 import PostItems from './components/PostItems.vue'
 import PostList from './components/PostList.vue'
+import PostTitle from './components/PostTitle.vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('PostItems', PostItems)
     app.component('PostList', PostList)
+  },
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-before': () => {
+        const { page } = useData()
+        if (page.value.relativePath.match(/^posts\/(?!index.md)/)) {
+          return h(PostTitle)
+        }
+      },
+    })
   },
 } satisfies Theme
